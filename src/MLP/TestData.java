3591dd09444906bb8d4f0864117b79ead63b2025
gareth <gareth.griffiths95@gmail.com>
2018-11-01 21:55:36 +0800

    asd

---
+++ src/MLP/TestData.java
@@ -0,0 +1,15 @@
+package MLP;
+
+public class TestData 
+{
+	float vals[] = new float[45];
+	char letter[] = new char[0];
+
+    public TestData() {}
+    
+    public TestData(float[] iVals, char[] iLetter)
+    {
+        vals = iVals;
+        letter = iLetter;
+    }
+}
